e914cd24fa8e971bcfb499b9218ee42f778fc394
Anastasiia Onufriieva <aonufriieva@silvery.ua>
2022-12-28 13:52:06 +0200

    Initial commit

---
+++ src/main/java/com/cib/bankidcommunication/BankIdCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.cib.bankidcommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIdCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankIdCommunicationApplication.class, args);
+    }
+
+}
